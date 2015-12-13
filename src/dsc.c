054620ce6d163c813e090e341c3b7cf4c71277be
calvinwilliams <calvinwilliams.c@gmail.com>
2015-12-13 21:02:10 +0800

    UPDATE TO V1.6.1B

--- src/dsc.c
+++ src/dsc.c
@@ -8,8 +8,8 @@
  * Licensed under the LGPL v2.1, see the file LICENSE in base directory.
  */
 
-char	__DIRECTSTRUCT_VERSION_1_6_0[] = "1.6.0" ;
-char	*__DIRECTSTRUCT_VERSION = __DIRECTSTRUCT_VERSION_1_6_0 ;
+char	__DIRECTSTRUCT_VERSION_1_6_1[] = "1.6.1" ;
+char	*__DIRECTSTRUCT_VERSION = __DIRECTSTRUCT_VERSION_1_6_1 ;
 
 int dsc( struct CommandParameter *pcmdparam )
 {
