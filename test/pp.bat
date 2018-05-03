9a08a902e932738e23b6b044e9456dd0d492aefe
calvinwilliams <calvinwilliams@163.com>
2018-05-03 20:00:09 +0800

    UPDATE TO V1.13.3B

--- test/pp.bat
+++
@@ -1,8 +0,0 @@
-dsc -f IDL_AllTypes.dsc -c-compact -c-compress -c-LOG
-dsc -f IDL_AllTypes2.dsc -c-compact -c-compress -c-LOG
-dsc -f IDL_CommHead.dsc -c-compact -c-LOG
-dsc -f IDL_BankTransaction.dsc -c-compress -c-LOG
-dsc -f IDL_BankXmlTransaction.dsc -c-xml -c-xml-compact -c-LOG
-dsc -f IDL_BankJsonTransaction.dsc -c-json -c-json-compact -c-LOG
-dsc -f IDL_testable.dsc -sql-pgsql -ec-pgsql -c-LOG
-pause
